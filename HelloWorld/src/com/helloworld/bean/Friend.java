dc60a2a8e87f3105724daae0303dd5bc426cf33e
TheHelloWorld <hundunhou@126.com>
2015-07-21 12:03:27 +0800

    test

---
+++ HelloWorld/src/com/helloworld/bean/Friend.java
@@ -0,0 +1,65 @@
+package com.helloworld.bean;
+
+public class Friend {
+	
+	private Long id;
+	
+	private String username;
+	
+	private String friendname;
+	
+	private Long userid;
+	
+	private Long firendid;
+	
+	private String status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFriendname() {
+		return friendname;
+	}
+
+	public void setFriendname(String friendname) {
+		this.friendname = friendname;
+	}
+
+	public Long getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+
+	public Long getFirendid() {
+		return firendid;
+	}
+
+	public void setFirendid(Long firendid) {
+		this.firendid = firendid;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
